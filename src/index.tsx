253022462796befef7688e37b40b760de61197ae
Necati Soyata <necati.soyata@gmail.com>
2021-07-28 23:26:09 +0300

    "setup unpkg routes"

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,53 @@
+import * as esbuild from "esbuild-wasm";
+import { useEffect } from "react";
+import { useState, useRef } from "react";
 import ReactDOM from "react-dom";
-
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 const App = () => {
-  return <h1>Hİ</h1>;
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    startService();
+  }, []);
+  const [input, setinput] = useState("");
+  const [code, setcode] = useState("");
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": "production",
+        global: "window",
+      },
+    });
+
+    setcode(result.outputFiles[0].text);
+  };
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setinput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
